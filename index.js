454c5ab872f08e22a8b05e3dab38c5a4343fec5e
Manuel Overdijk <manuel.overdijk@gmail.com>
2015-05-05 15:18:44 +0200

    license

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ This Source Code is licensed under the MIT license. If a copy of the
+ MIT-license was not distributed with this file, You can obtain one at:
+ http://opensource.org/licenses/mit-license.html.
+ @author: Manuel Overdijk (manueloverdijk)
+ @license MIT
+ @copyright Manuel Overdijk, 2015
+ */
+
+var MyoBluetooth = require('./lib/agent');
+
+module.exports = new MyoBluetooth();
+
